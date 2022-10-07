7ef63dd8caa40e4a3a951de8276f1c3c321bafad
Daniil Novitsky <ndi@omegasoftware.eu>
2022-10-08 01:18:52 +0400

    feat: updated and fixed deprecated code

--- client/src/app/shared/validators/equal.validator.ts
+++ client/src/app/shared/validators/equal.validator.ts
@@ -1,7 +1,7 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export function EqualValidator(controlName: string): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } => {
+  return (control: AbstractControl): { [key: string]: unknown } => {
     const compareControl = control.parent ? control.parent.get(controlName) : null;
     const areEqual = compareControl && control.value === compareControl.value;
     return areEqual ? null : { notEqual: true };
